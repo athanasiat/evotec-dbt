ee79f591206b95338fafe6a7285e9ad30bb9f4ee
Athanasia Tsikriki <athanasia.t@snapanalytics.co.uk>
2024-06-27 07:49:07 +0000

    update snapshot schema

--- snapshots/sandbox/snapshot_quoteline_at.sql
+++ snapshots/sandbox/snapshot_quoteline_at.sql
@@ -3,7 +3,7 @@
 
 {{
     config(
-        target_schema='DW_L01_STG_ONE_CRM_DBT',
+        target_schema='DW_L02_PSH_ONE_CRM_DBT',
         unique_key='ID',
         strategy='timestamp',
         updated_at='_MODIFIED'
